f95d1c6f5a0ef40dcd3d6b48af3c4770f823109c
phs1234 <timemarket777@naver.com>
2020-12-15 00:48:35 +0900

    맵 화면전환 완성

--- app/src/main/java/com/devreport/cloud/main/fragment/weather/WeatherFragment.java
+++ app/src/main/java/com/devreport/cloud/main/fragment/weather/WeatherFragment.java
@@ -1,5 +1,6 @@
 package com.devreport.cloud.main.fragment.weather;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -7,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -18,46 +20,103 @@ import com.firebase.cloud.R;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class WeatherFragment extends Fragment {
-    public static final String TAG = "WeatherFragment";
+    private static final String TAG = "WeatherFragment";
 
-    public View view;
+    private static final String APPID = "dacdd3ff23cc19ea41e9107e3006a07e";
+    private static final int MAP_REQUEST_CODE = 101;
+
+    private View view;
+
+    private TextView cityNameTextView;
+
+    private ImageView cloudImageView;
+    private TextView cloudTextView, cloudSubTextView;
+
+    private TextView  tempTextView, humidityTextView, pressureTextView, windTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // 뷰 관련 객체 선언
         LayoutInflater layoutInflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         view = layoutInflater.inflate(R.layout.fragment_weather, null);
 
         LinearLayout mapButton = view.findViewById(R.id.MapButton);
 
-        TextView tempTextView = view.findViewById(R.id.TempTextView);
-        TextView humidityTextView = view.findViewById(R.id.HumidityTextView);
-        TextView pressureTextView = view.findViewById(R.id.PressureTextView);
-        TextView windTextView = view.findViewById(R.id.WindTextView);
+        cityNameTextView = view.findViewById(R.id.CityNameTextView);
+
+        cloudImageView = view.findViewById(R.id.CloudImageView);
+        cloudTextView = view.findViewById(R.id.CloudTextView);
+        cloudSubTextView = view.findViewById(R.id.CloudSubTextView);
+
+        tempTextView = view.findViewById(R.id.TempTextView);
+        humidityTextView = view.findViewById(R.id.HumidityTextView);
+        pressureTextView = view.findViewById(R.id.PressureTextView);
+        windTextView = view.findViewById(R.id.WindTextView);
 
         mapButton.setOnClickListener(view -> {
             Intent intent = new Intent(getContext(), MapActivity.class);
-            startActivity(intent);
+            startActivityForResult(intent, MAP_REQUEST_CODE);
         });
 
-        WeatherRetrofitService.getCurrentWeather(1835847, new Callback<WeatherAllData>() {
+        // 날씨 서울로 세팅
+        setCurrentWeather(1835847);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        if (requestCode == MAP_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                cityNameTextView.setText(intent.getExtras().getString("name"));
+
+                setCurrentWeather(intent.getExtras().getInt("id"));
+            }
+        }
+    }
+
+    // API 에서 불러와서 화면에 보여주기 까지
+    private void setCurrentWeather (int id) {
+        WeatherInterface weatherInterface = new Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(WeatherInterface.class);
+
+        Call<WeatherAllData> call =  weatherInterface.getCurrentWeather(id, APPID);
+        call.enqueue(new Callback<WeatherAllData>() {
             @Override
             public void onResponse(Call<WeatherAllData> call, Response<WeatherAllData> response) {
                 WeatherAllData result = response.body();
 
+                // 구름 이미지 및 텍스트 변경
+                if (result.getCloudData().getDensity() >= 60) {
+                    cloudImageView.setImageResource(R.drawable.icon_cloud_cloudy);
 
+                    cloudTextView.setText("Cloudy Day");
+                    cloudSubTextView.setText("산책하기 좋은 날씨네요!");
+                } else if (result.getCloudData().getDensity() >= 30) {
+                    cloudImageView.setImageResource(R.drawable.icon_cloud_calm);
 
-                String message = "구름 : " + result.getCloudData().getDensity() + "\n"
-                                + "온도 : " + result.getDetailData().getTemp() + "\n"
-                                + "습기 : " + result.getDetailData().getHumidity() + "\n"
-                                + "기압 : " + result.getDetailData().getPressure() + "\n"
-                                + "바람 : " + result.getWindData().getSpeed() + "\n";
+                    cloudTextView.setText("Calm Day");
+                    cloudSubTextView.setText("아메리카노가 땡기네요!");
+                } else {
+                    cloudImageView.setImageResource(R.drawable.icon_cloud_clear);
 
-                Log.d(TAG, message);
+                    cloudTextView.setText("Clear Day");
+                    cloudSubTextView.setText("뭘 해도 좋을 날씨예요!");
+                }
 
+                // 세부사항 바꾸기
                 tempTextView.setText((int)(result.getDetailData().getTemp() - 273) + " °C");
                 humidityTextView.setText(result.getDetailData().getHumidity() + " %");
                 pressureTextView.setText(result.getDetailData().getPressure() + " inHg");
@@ -71,8 +130,15 @@ public class WeatherFragment extends Fragment {
         });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return view;
+    // API 에서 날씨 데이터를 가져와서 CallBack 에 넘김
+    public void setCurrentWeather (int id, Callback callback) {
+        WeatherInterface weatherInterface = new Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(WeatherInterface.class);
+
+        Call<WeatherAllData> call =  weatherInterface.getCurrentWeather(id, APPID);
+        call.enqueue(callback);
     }
 }
--- app/src/main/java/com/devreport/cloud/map/MapActivity.java
+++ app/src/main/java/com/devreport/cloud/map/MapActivity.java
@@ -1,20 +1,36 @@
 package com.devreport.cloud.map;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.cloud.R;
-import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    private static final String TAG = "MapActivity";
+
+    private TextView nameTextView, latLngTextView, descTextView;
+    private Button setButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,26 +38,72 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.MapFragment);
         mapFragment.getMapAsync(this);
+
+        nameTextView = findViewById(R.id.NameTextView);
+        latLngTextView = findViewById(R.id.LatLngTextView);
+        descTextView = findViewById(R.id.DescTextView);
+
+        setButton = findViewById(R.id.setButton);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        try {
+            // 맵 스타일 지정
+            googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.map_style));
 
+            // 마커 표시하기
+            InputStream inputStream = getAssets().open("map_city.json");
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+            int fileSize = inputStream.available();
 
-        MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("한국의 수도");
+            byte[] buffer = new byte[fileSize];
+            inputStream.read(buffer);
+            inputStream.close();
 
-        googleMap.addMarker(markerOptions);
+            JSONArray cityArray = new JSONArray(new String(buffer, "UTF-8"));
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 1));
+            for (int i = 0; i < cityArray.length(); i++) {
+                JSONObject city = cityArray.getJSONObject(i);
+                JSONObject cityCoord = city.getJSONObject("coord");
+
+                MarkerOptions markerOptions = new MarkerOptions();
+                MapMarkerInfo mapMarkerInfo = new MapMarkerInfo(city.getInt("id"),
+                                                                city.getString("name"),
+                                                                cityCoord.getDouble("lat"),
+                                                                cityCoord.getDouble("lon"),
+                                                                "");
+
+                markerOptions.position(new LatLng(mapMarkerInfo.getLat(), mapMarkerInfo.getLon()));
+                googleMap.addMarker(markerOptions).setTag(mapMarkerInfo);
+            }
+
+            googleMap.setOnMarkerClickListener(this);
+        } catch (IOException exception) {
+
+        } catch (JSONException exception) {
+            exception.printStackTrace();
+        }
     }
 
     @Override
     public boolean onMarkerClick(Marker marker) {
+        MapMarkerInfo mapMarkerInfo = (MapMarkerInfo) marker.getTag();
+
+
+        nameTextView.setText(mapMarkerInfo.getName());
+        latLngTextView.setText("Lat " + String.format("%.2f", mapMarkerInfo.getLat()) + "\t" + "Lon " + String.format("%.2f", mapMarkerInfo.getLon()));
+        descTextView.setText(mapMarkerInfo.getDesc());
+
+        setButton.setOnClickListener(view -> {
+            Intent intent = new Intent();
+
+            intent.putExtra("id", mapMarkerInfo.getId());
+            intent.putExtra("name", mapMarkerInfo.getName());
+
+            setResult(RESULT_OK, intent);
+            finish();
+        });
 
         return true;
     }
